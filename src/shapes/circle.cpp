6d931bea1c4e37c0da025ce8b09f20b953ec9ba7
Romain MARTI <romain.marti@universite-paris-saclay.fr>
2026-05-12 09:58:13 +0200

    Ajout de la fonction circumference pour calculer la circonférence d'un cercle

--- src/shapes/circle.cpp
+++ src/shapes/circle.cpp
@@ -1,4 +1,10 @@
 #include "point.hpp"
 #include "shapes/circle.hpp"
 
+const float pi = 3.141592;
+
 Circle::Circle(double r, Point c) : radius(r), center(c) {};
+
+double Circle::circumference() {
+	return 2*radius*pi;
+}
