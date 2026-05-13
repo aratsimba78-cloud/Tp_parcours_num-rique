5ea51ff42556071db28fa00d238ab6c0df686a8d
Romain MARTI <romain.marti@universite-paris-saclay.fr>
2026-05-13 15:02:29 +0200

    Ajout de la fonction resize pour modifier la taille uniquement du cercle par un réel

--- src/shapes/circle.cpp
+++ src/shapes/circle.cpp
@@ -1,5 +1,9 @@
 #include "point.hpp"
 #include "shapes/circle.hpp"
+#include "draw.hpp"
+#include <math.h>
+#include <vector>
+#include<iostream>
 
 const float pi = 3.141592;
 
@@ -11,4 +15,38 @@ double Circle::circumference() {
 
 double Circle::area() {
 	return pi*radius*radius;
+}
+
+void Circle::draw() {
+	std::vector<Point> cercle;
+	
+	Point C,A;
+	C.x = center.x - radius;
+	C.y = center.y;
+	
+	cercle.push_back(C);
+	
+	for (double i = C.x ; i < center.x + radius ; ++i){
+		double dx = i - center.x;
+		A.y = center.y + sqrt(radius*radius - dx*dx);
+		A.x = i;
+		cercle.push_back(A);
+	}
+	
+	for (double i = center.x + radius ; i >= center.x - radius ; --i){
+		double dx = i - center.x;
+		A.y = center.y - sqrt(radius*radius - dx*dx);
+		A.x = i;
+		cercle.push_back(A);
+	}
+	draw_picture(cercle);	
+}
+
+void Circle::translate(Point T) {
+	center.x += T.x;
+	center.y += T.y;
+}
+
+void Circle::resize(double ratio) {
+	radius *= ratio;
 }
