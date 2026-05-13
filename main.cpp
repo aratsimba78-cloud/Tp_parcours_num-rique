508e51d606c516e1732c2a8577f8ef4ca6c98723
Romain MARTI <romain.marti@universite-paris-saclay.fr>
2026-05-13 14:54:51 +0200

    Ajout des fonctions draw pour dessiner le cercle et translate pour ajouter les coordonnées d'un point à celles du centre du cercle

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include "draw.hpp"
 #include "point.hpp"
 #include "shapes.hpp"
+#include "shapes/circle.hpp"
 #include <vector>
 
+#include<iostream>
 
 int main() {
 	
+	Circle C1 {
+		10,
+		Point(0,0),
+	};
+	
+	Point X {
+		1, 1,
+	};
+	
+	/*
+	std::cout<<C1.circumference()<<std::endl;
+	std::cout<<C1.area();*/
+	//C1.draw();
+	
+	/*
 	// Define points in a vector
 	std::vector<Point> points = {
 		Point(0, 0),
@@ -18,6 +35,6 @@ int main() {
 	
 	// Draw the picture by connecting the points
 	draw_picture(points);
-	
+	*/
 	return 0;
 }
